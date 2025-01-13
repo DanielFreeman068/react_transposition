a13f03b0f65a52a03b3398b21c86030872033a4b
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-01-13 15:39:05 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 })
 
 // Middleware
